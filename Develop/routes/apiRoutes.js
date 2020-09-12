a1b4bdff9b1ef2918e4644b097376ddc3393cce5
sheikb08 <sheikb08@aol.com>
2020-09-12 14:45:56 -0700

    restructured routes

--- Develop/routes/apiRoutes.js
+++ Develop/routes/apiRoutes.js
@@ -6,6 +6,7 @@ module.exports = function(app){
 
 app.get('api/notes', function(req, res){
     let notes = JSON.parse(fs.readFileSync(".db/db.json", "utf8"));
+    console.log("\nGET request - Returning notes data: " + JSON.stringify(notes));
     res.json(notes);
 });
 
@@ -14,6 +15,7 @@ app.post('api/notes', function(req, res){
     let notes = JSON.parse(fs.writeFileSync(".db/db.json", "utf8"));
     console.log(newNotes);
     notes.push(newNotes);
+    fs.writeFileSync('./db/db.json', JSON.stringify(notes));
     res.json(notes);
 });
 
--- Develop/server.js
+++ Develop/server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 
-
 const app = express();
+
 const PORT = process.env.PORT || 8080;
 
 // Sets up the Express app to handle data parsing
@@ -9,15 +9,10 @@ app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.get('/notes', function (req, res) {
-    res.sendFile(path.join(__dirname, './public/notes.html'));
-  });
-
-  app.get('/', function (req, res) {
-    res.sendFile(path.join(__dirname, './public/index.html'));
-  });
+require('./routes/apiRoutes')(app);
+require('./routes/htmlRoutes')(app);
 
 
-  app.listen(PORT, function () {
+app.listen(PORT, function () {
     console.log(`App listening on PORT: ${PORT}`);
   });
