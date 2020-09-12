ca3efdaf37120ddddbc22d2328f7108e75fec941
sheikb08 <sheikb08@aol.com>
2020-09-12 13:36:54 -0700

    added notes

--- Develop/server.js
+++ Develop/server.js
@@ -1,16 +1,20 @@
-const express = require("require");
-const fs = require("fs");
-const path = require("path");
+const express = require("express");
+
 
 const app = express();
 const PORT = process.env.PORT || 8080;
 
 // Sets up the Express app to handle data parsing
+app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.get('/notes', function (req, res) {
-    res.sendFile(path.join(__dirname, 'notes.html'));
+    res.sendFile(path.join(__dirname, './public/notes.html'));
+  });
+
+  app.get('/', function (req, res) {
+    res.sendFile(path.join(__dirname, './public/index.html'));
   });
 
 
